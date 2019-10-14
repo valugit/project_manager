bc8b390f0701e6e332aa600dfd9296fc785a7159
Alice Bonnet <38134449+Aelyce11@users.noreply.github.com>
2019-10-14 14:51:38 +0200

    update models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app import models
 
 # Register your models here.
+admin.site.register(models.Project)
+admin.site.register(models.Student)
+admin.site.register(models.Course)
--- app/models.py
+++ app/models.py
@@ -1,18 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import datetime
-
-class Project(models.Model):
-    title: models.CharField(max_length=100)
-    statement: models.CharField(max_length=300)
-    date_start: models.DateTimeField(auto_now_add=True)
-    date_end: models.DateTimeField(auto_now=False, auto_now_add=False)
-    date_deadline: models.DateTimeField(auto_now=False, auto_now_add=False)
-
-class Student(models.Model):
-    name: models.CharField(max_length=100)
-    year: models.IntegerField(validators=[MaxValueValidator(datetime.datetime.now().year+3), MinValueValidator(2014)])
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Course(models.Model):
-    title: models.CharField(max_length=100)
-    year: models.IntegerField
-    teacher: models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    year = models.IntegerField
+    teacher = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    statement = models.CharField(max_length=300)
+    date_start = models.DateTimeField(auto_now_add=True)
+    date_end = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True)
+    date_deadline = models.DateTimeField(auto_now=False, auto_now_add=False)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    desc = models.TextField(max_length=500, blank=True)
+    year = models.IntegerField(
+        validators=[
+            MaxValueValidator(datetime.datetime.now().year + 5),
+            MinValueValidator(2014),
+        ]
+    )
+    # users = User.objects.all().select_related('profile')
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
