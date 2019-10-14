b3426d71c6d2f1daf6d856a642ecca14d882caf0
Alice Bonnet <38134449+Aelyce11@users.noreply.github.com>
2019-10-14 12:30:42 +0200

    update models

--- app/models.py
+++ app/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+import datetime
 
-# Create your models here.
 class Project(models.Model):
-    project_title: models.CharField(max_length=100)
-    project_statement: models.CharField(max_length=300)
+    title: models.CharField(max_length=100)
+    statement: models.CharField(max_length=300)
     date_start: models.DateTimeField(auto_now_add=True)
     date_end: models.DateTimeField(auto_now=False, auto_now_add=False)
+    date_deadline: models.DateTimeField(auto_now=False, auto_now_add=False)
+
+class Student(models.Model):
+    name: models.CharField(max_length=100)
+    year: models.IntegerField(validators=[MaxValueValidator(datetime.datetime.now().year+3), MinValueValidator(2014)])
+
+class Course(models.Model):
+    title: models.CharField(max_length=100)
+    year: models.IntegerField
+    teacher: models.CharField(max_length=100)
