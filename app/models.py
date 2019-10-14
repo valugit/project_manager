b9d7d0cd403f2fb88a0f3e1bf7ab5542f6786ed5
valugit <44845228+valugit@users.noreply.github.com>
2019-10-14 11:33:03 +0200

    Project Model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    project_title: models.CharField(max_length=100)
+    project_statement: models.CharField(max_length=300)
+    date_start: models.DateTimeField(auto_now_add=True)
+    date_end: models.DateTimeField(auto_now=False, auto_now_add=False)
