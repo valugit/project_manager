de9dce737a993a02af7ce24dfb03a79cc369503f
valugit <44845228+valugit@users.noreply.github.com>
2019-10-27 19:01:34 +0100

    API update

--- app/api.py
+++ app/api.py
@@ -1,8 +1,34 @@
-from app.models import Student
+from app.models import Student, Teacher, Course, Project
 from rest_framework import serializers
 
 
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Student
-        fields = ["url", "email", "desc", "year"]
+        fields = ["url", "email", "year"]
+
+
+class TeacherSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = ["url", "first_name", "last_name"]
+
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Course
+        fields = ["url", "title", "year", "teacher_id"]
+
+
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+        fields = [
+            "url",
+            "title",
+            "statement",
+            "date_start",
+            "date_deadline",
+            "course_id",
+        ]
+
