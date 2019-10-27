5c5da96c743a44e6552df4702b5628bae24c6532
valugit <44845228+valugit@users.noreply.github.com>
2019-10-27 16:31:33 +0100

    admin route

--- project_manager/urls.py
+++ project_manager/urls.py
@@ -25,6 +25,6 @@ router.register(r"students", views.StudentViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include(router.urls)),
+    path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
